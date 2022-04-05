f301651ea8c156a8b027f456fca74c2dc37405fa
tokyo3751 <tomizawa3751@gmail.com>
2022-04-05 10:24:40 +0000

    [Add]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,23 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+       render :edit
+    else
+       redirect_to books_path
+    end
+  end
+
+  def update
+   @book = Book.find(params[:id])
+   @book.user_id = current_user.id
+   if @book.update(book_params)
+     flash[:notice] = "You have updated book successfully."
+     redirect_to book_path(@book.id)
+   else
+     render :edit
+   end
   end
 
   def create
@@ -23,6 +40,14 @@ class BooksController < ApplicationController
       @user = current_user
       @books = Book.all
       render :index
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,26 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = Book.new
+    @books = Book.where(user_id: @user.id)
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   private
